7e27d9bc712f2dd2855bb60d71cbb8675a8135db
khunz <arjun.iceburnt@gmail.com>
2026-04-23 22:05:04 +0100

    Added the operate function to call the operations

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// Creating variables for the display
+let numOne = 0,
+  operator = "",
+  numTwo = 0;
+
+// Operate function
+function operate(a, b, op) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
